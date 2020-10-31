9e91d8982cb7df31bc984addabef8059e52467fe
Aaditya jha <aadityajha2000@gmail.com>
2020-10-31 15:39:51 +0545

    Initial Setup

---
+++ src/components/MainComponent.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Main extends Component{
+    constructor() {
+        super(props);
+    }
+
+    render() {
+        return(
+            <div>
+                <h2>This is a Main Page</h2>
+            </div>
+        );
+    }
+}
