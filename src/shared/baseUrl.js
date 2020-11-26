11f5bc94683c69fc4543bf641d79b28fe7ad86f8
Aaditya jha <aadityajha2000@gmail.com>
2020-11-26 21:13:17 +0545

    BaseUrl configure

--- src/shared/baseUrl.js
+++ src/shared/baseUrl.js
@@ -1,6 +1,6 @@
-// export const baseUrl = "https://infinite-garden-80024.herokuapp.com/";
+export const baseUrl = "https://infinite-garden-80024.herokuapp.com/";
 
-export const baseUrl = "http://localhost:5000/";
+// export const baseUrl = "http://localhost:5000/";
 
 // export const firebaseUrl =
 //   "https://console.firebase.google.com/project/book-store-d4cfd/storage/book-store-d4cfd.appspot.com/files";
