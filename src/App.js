e1bb3a7e58fa4cfb48c5a968ed7648428f643ddf
Aaditya jha <aadityajha2000@gmail.com>
2020-11-27 15:43:44 +0545

    Comment feature corrected

--- src/App.js
+++ src/App.js
@@ -3,20 +3,17 @@ import Main from "./components/MainComponent";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { ConfigureStore } from "./redux/configureStore";
-import { CookiesProvider } from "react-cookie";
 
 const store = ConfigureStore();
 
 function App() {
   return (
     <div>
-      <CookiesProvider>
-        <Provider store={store}>
-          <BrowserRouter>
-            <Main />
-          </BrowserRouter>
-        </Provider>
-      </CookiesProvider>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Main />
+        </BrowserRouter>
+      </Provider>
     </div>
   );
 }
