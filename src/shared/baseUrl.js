479c1c420bf553920f54bafcffdb79edfe25cef5
Aaditya Jha <aadityajha2000@gmail.com>
2020-12-02 20:00:05 +0545

    Url for backend changed

--- src/shared/baseUrl.js
+++ src/shared/baseUrl.js
@@ -1,4 +1,4 @@
-export const baseUrl = "https://infinite-garden-80024.herokuapp.com/";
+export const baseUrl = "https://secure-savannah-23740.herokuapp.com/";
 
 // export const baseUrl = "http://localhost:5000/";
 
