c72a2313b5d2f99c0aba6d6c5c388544e0e70f54
Aaditya Jha <aadityajha2000@gmail.com>
2020-12-06 20:23:32 +0545

    Home page bg image

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,13 +1,20 @@
-import React from 'react';
-
-
+import React from "react";
 
 function Home(props) {
-    return(
-        <div className="container">
-            <h2>Home</h2>
-        </div>
-    )
+  return (
+    <div
+      id="home"
+      //   style={{
+      //     height: "100vh",
+      //     width: "100%",
+      //     backgroundImage: `url('${process.env.PUBLIC_URL}/bookshelf.jpg')`,
+      //   }}
+    >
+      <div id="hero" className="container">
+        <h2>Home</h2>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
